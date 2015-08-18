e1339faaca46f21dbedfaf934399951e6f67dacf
danielgiovanni <vanvandg@gmail.com>
2015-08-18 15:02:39 +0700

    Add default test spec and karma conf

---
+++ src/client/test/default.spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by daniel.g.gunawan on 7/22/2015.
+ */
+/* jshint -W117 */
+describe('A suite', function() {
+    it('contains spec with an expectation', function() {
+        expect(true).toBe(true);
+    });
+});
